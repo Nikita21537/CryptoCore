97c047f14f54030cd4239001bf276f72ceef8591
Nikita21537 <ddererw2002ert@gmail.com>
2025-11-23 22:34:12 +0300

    Create ofb.py

---
+++ ofb.py
@@ -0,0 +1,44 @@
+import os
+from src.modes.mode_interface import ModeInterface
+
+class OFBCipher(ModeInterface):
+    def __init__(self, cipher, block_size=16):
+        self.cipher = cipher
+        self.block_size = block_size
+
+    def encrypt(self, plaintext):
+        iv = os.urandom(self.block_size)
+        ciphertext = b''
+        keystream = iv
+        
+        for i in range(0, len(plaintext), self.block_size):
+            block = plaintext[i:i+self.block_size]
+            encrypted_keystream = self.cipher.encrypt(keystream)
+            
+            # XOR с открытым текстом
+            encrypted_block = bytes(a ^ b for a, b in zip(block, encrypted_keystream[:len(block)]))
+            ciphertext += encrypted_block
+            
+            # Для следующего блока используем зашифрованный keystream
+            keystream = encrypted_keystream
+
+        return iv + ciphertext
+
+    def decrypt(self, ciphertext):
+        iv = ciphertext[:self.block_size]
+        ciphertext = ciphertext[self.block_size:]
+        plaintext = b''
+        keystream = iv
+        
+        for i in range(0, len(ciphertext), self.block_size):
+            block = ciphertext[i:i+self.block_size]
+            encrypted_keystream = self.cipher.encrypt(keystream)
+            
+            # XOR с шифротекстом
+            decrypted_block = bytes(a ^ b for a, b in zip(block, encrypted_keystream[:len(block)]))
+            plaintext += decrypted_block
+            
+            # Для следующего блока используем зашифрованный keystream
+            keystream = encrypted_keystream
+
+        return plaintext
